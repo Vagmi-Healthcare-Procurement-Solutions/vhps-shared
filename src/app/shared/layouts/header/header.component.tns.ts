28cf1a3c651a6010caf498a3006af40ee7a78391
Garcia, Omar <ogarciamg@outlook.com>
2020-04-17 06:32:53 +0000

    Adding phone calls

--- src/app/shared/layouts/header/header.component.tns.ts
+++ src/app/shared/layouts/header/header.component.tns.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as TNSPhone from 'nativescript-phone';
 
 @Component({
   selector: 'app-header',
@@ -13,4 +14,10 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  public callNow() {
+    const phoneNumber = '1234567890';
+    TNSPhone.requestCallPermission('You should accept the permission to be able to make a direct phone call.')
+       .then(() => TNSPhone.dial(phoneNumber, false))
+       .catch(() => TNSPhone.dial(phoneNumber, true));
+ }
 }
