21b31bbbe0d54eb9473a50c8760869697425f2a3
Garcia, Omar <ogarciamg@outlook.com>
2020-04-19 22:47:54 +0000

    Fixes after review

--- src/app/shared/layouts/header/header.component.tns.ts
+++ src/app/shared/layouts/header/header.component.tns.ts
@@ -16,8 +16,6 @@ export class HeaderComponent implements OnInit {
 
   public callNow() {
     const phoneNumber = '1234567890';
-    TNSPhone.requestCallPermission('You should accept the permission to be able to make a direct phone call.')
-       .then(() => TNSPhone.dial(phoneNumber, false))
-       .catch(() => TNSPhone.dial(phoneNumber, true));
+    TNSPhone.dial(phoneNumber, true);
  }
 }
